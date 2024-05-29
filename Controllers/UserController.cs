e6271baaecfd1fb74cd60b3db2bb110c95324bfa
github_cloudlabsuser_2073@cloudlabsaiuser.com <github_cloudlabsuser_2073@cloudlabsaiuser.com>
2024-05-29 10:55:11 +0000

    Updated project files, user controller, and web config
    Updated `CRUD application 2.csproj` to temporarily include and later remove a reference to `Microsoft.CodeDom.Providers.DotNetCompilerPlatform`. Removed a comment from the end of the file. Upgraded Visual Studio version from 16 to 17 in `CRUD application 2.sln` and added a new project reference to `UnitTests` with its configuration settings. Implemented methods for Index, Details, Create, Edit, and Delete actions in `UserController.cs` to handle user data in a static list. Updated the binding redirect for `WebGrease` in `Web.config`.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,66 +1,108 @@
 using CRUD_application_2.Models;
 using System.Linq;
 using System.Web.Mvc;
- 
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
     {
+        // Static list to store user data
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+
         // GET: User
+        // Returns a view with a list of all users
         public ActionResult Index()
         {
-            // Implement the Index method here
+            return View(userlist);
         }
- 
+
         // GET: User/Details/5
+        // Returns a view with the details of the user with the given id
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // GET: User/Create
+        // Returns a view to create a new user
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
- 
+
         // POST: User/Create
+        // Creates a new user and redirects to the index view
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
- 
+
         // GET: User/Edit/5
+        // Returns a view to edit the user with the given id
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
+        // Edits the user with the given id and redirects to the index view
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
- 
+
         // GET: User/Delete/5
+        // Returns a view to confirm the deletion of the user with the given id
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Delete/5
+        // Deletes the user with the given id and redirects to the index view
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
     }
 }
