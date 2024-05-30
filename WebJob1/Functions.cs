e66000986380200220c695fc898725c074c83ed0
github_cloudlabsuser_2081@cloudlabsaiuser.com <github_cloudlabsuser_2081@cloudlabsaiuser.com>
2024-05-30 10:52:40 +0000

    a

---
+++ WebJob1/Functions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob1
+{
+    public class Functions
+    {
+        // This function will be triggered based on the schedule you have set for this WebJob
+        // This function will enqueue a message on an Azure Queue called outputqueue
+        [NoAutomaticTrigger]
+        public static void CreateQueueMessage([Queue("outputqueue")] out string message, string value, ILogger logger)
+        {
+            message = value;
+            logger.LogInformation("Creating queue message: ", message);
+        }
+    }
+}
---
+++ WebJob1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebJob1
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?linkid=2250384
+    internal class Program
+    {
+        // Please set AzureWebJobsStorage connection strings in appsettings.json for this WebJob to run.
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices()
+                    .AddAzureStorageQueues();
+                })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Information);
+                    b.AddConsole();
+                });
+
+
+            var host = builder.Build();
+            using (host)
+            {
+                // The following code will invoke a function called CreateQueueMessage and
+                // pass in data (value in this case) to the function
+                var jobHost = host.Services.GetService(typeof(IJobHost)) as JobHost;
+                var inputs = new Dictionary<string, object>
+            {
+                { "value", "Hello world!" }
+            };
+
+                await host.StartAsync();
+                await jobHost.CallAsync("CreateQueueMessage", inputs);
+                await host.StopAsync();
+            }
+        }
+    }
+}
+
+
