c33c26b7adddc8d41253a0ec1f4f522de1c5ab79
github_cloudlabsuser_1957@cloudlabsaiuser.com <github_cloudlabsuser_1957@cloudlabsaiuser.com>
2024-06-06 09:53:13 +0000

    crud user

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace CRUD_application_2.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
+        // Add custom validation properties or methods here
     }
 }
