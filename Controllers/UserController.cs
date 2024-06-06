d6e70f326e8f47ed2b71949a4a7ed957fc997102
github_cloudlabsuser_2033@cloudlabsaiuser.com <github_cloudlabsuser_2033@cloudlabsaiuser.com>
2024-06-06 09:48:54 +0000

    Updated packages, added unit tests, and improved UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,60 +7,124 @@ namespace CRUD_application_2.Controllers
     public class UserController : Controller
     {
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
-        // GET: User
+
+        /// <summary>
+        /// Retrieves the list of users and displays them in the Index view.
+        /// </summary>
+        /// <returns>The Index view with the list of users.</returns>
         public ActionResult Index()
         {
-            // Implement the Index method here
+            return View(userlist);
         }
- 
-        // GET: User/Details/5
+
+        /// <summary>
+        /// Retrieves the details of a specific user based on the provided ID and displays them in the Details view.
+        /// </summary>
+        /// <param name="id">The ID of the user to retrieve.</param>
+        /// <returns>The Details view with the details of the user.</returns>
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
- 
-        // GET: User/Create
+
+        /// <summary>
+        /// Displays the view to create a new user.
+        /// </summary>
+        /// <returns>The Create view.</returns>
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
- 
-        // POST: User/Create
+
+        /// <summary>
+        /// Handles the HTTP POST request to create a new user.
+        /// </summary>
+        /// <param name="user">The user object containing the information of the new user.</param>
+        /// <returns>Redirects to the Index action to display the updated list of users.</returns>
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            userlist.Add(user);
+            return RedirectToAction("Index");
         }
- 
-        // GET: User/Edit/5
+
+        /// <summary>
+        /// Displays the view to edit an existing user with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to edit.</param>
+        /// <returns>The Edit view with the user to edit.</returns>
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            User user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
-        // POST: User/Edit/5
+
+        /// <summary>
+        /// Handles the HTTP POST request to update an existing user with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to update.</param>
+        /// <param name="user">The user object containing the updated information.</param>
+        /// <returns>Redirects to the Index action to display the updated list of users.</returns>
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            User existingUser = userlist.FirstOrDefault(x => x.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+
+            return RedirectToAction("Index");
         }
- 
-        // GET: User/Delete/5
+
+        /// <summary>
+        /// Displays the view to delete an existing user with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
+        /// <returns>The Delete view with the user to delete.</returns>
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            User user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
-        // POST: User/Delete/5
+
+        /// <summary>
+        /// Handles the HTTP POST request to delete an existing user with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
+        /// <param name="collection">The form collection.</param>
+        /// <returns>Redirects to the Index action to display the updated list of users.</returns>
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            User user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Remove(user);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Retrieves the list of users that match the provided search term and displays them in the Search view.
+        /// </summary>
+        /// <param name="searchTerm">The search term to match against user names.</param>
+        /// <returns>The Search view with the list of matching users.</returns>
+        public ActionResult Search(string searchTerm)
+        {
+            var searchResults = userlist.Where(user => user.Name.Contains(searchTerm)).ToList();
+            return View(searchResults);
         }
     }
 }
